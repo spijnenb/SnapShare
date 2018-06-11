fa5434d127627b2aa02e023fef54b07cfb2230b5
DeSteef <stefanpijnenburg@outlook.com>
2018-06-11 20:25:30 +0200

    add file uploads

--- app.js
+++ app.js
@@ -1,9 +1,51 @@
 var express 	= require("express"),
-	app 		= express(),
-	bodyParser 	= require("body-parser"),
+	app			= express(),
 	mongoose 	= require("mongoose"),
+	multer 		= require("multer"),
 	Snap 		= require("./models/snap"),
 	seedDB 		= require("./seed");
+
+// setup file uploads
+
+var storage = multer.diskStorage({
+	destination: function(req, file, cb){
+		cb(null, './uploads');
+	},
+	filename: function(req, file, cb){
+		var filename = simpleHash();
+		if (file.mimetype === "image/jpeg") {
+			cb(null, filename + ".jpg");
+		} else if (file.mimetype === "image/png") {
+			cb(null, filename + ".png");
+		}
+	}
+})
+
+var upload = multer({
+	storage, storage, 
+	limits:{
+		fileSize: 1024 * 512		// 500kb
+	},
+	fileFilter: fileFilter
+});
+
+function fileFilter(req, file, cb) {
+	if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+		cb(null,true);
+	} else {
+		cb(null, false);
+	}
+}
+
+function simpleHash() {
+	var letters = "abcdefghijklmnopqrstuvwxyz";
+	var hash = Date.now().toString(16);
+	for (var i = 0; i < 4; i++) {
+		var rng = Math.floor(Math.random() * hash.length + 1);
+		hash += letters.charAt(rng);
+	}
+	return hash;
+}
 
 // seedDB();
 
@@ -11,10 +53,10 @@ var express 	= require("express"),
 process.env.IP = process.env.IP || "localhost";
 process.env.PORT = process.env.PORT || 3000;
 
-// express config
+// CONFIG
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use("/uploads", express.static("uploads"));
 mongoose.connect("mongodb://localhost/snapshare");
 
 // ROUTES
@@ -32,7 +74,6 @@ app.get("/snaps", function(req, res){
 			res.render("index", {snaps:snaps});
 		}
 	});
-	
 });
 
 // NEW ROUTE
@@ -41,12 +82,16 @@ app.get("/snaps/new", function(req, res){
 });
 
 // CREATE ROUTE
-app.post("/snaps", function(req, res){
-	// add snap object with imgurl attribute
-	Snap.create(req.body.snap, function(err, addedSnap){
-		if (err) console.log(err);
+app.post("/snaps", upload.single('image'), function(req, res){
+	var savedImage = (req.file) ? req.file.path : req.body.imgurl;
+	if (savedImage) {
+		Snap.create({imgurl:savedImage}, function(err, addedSnap){
+			if (err) console.log(err);
+			res.redirect("/snaps");
+		});
+	} else {
 		res.redirect("/snaps");
-	});
+	}	
 });
 
 app.get('*', function(res, res){
--- models/snap.js
+++ models/snap.js
@@ -2,7 +2,8 @@ var mongoose = require("mongoose");
 
 var snapSchema = new mongoose.Schema({
 	author: String,
-	imgurl: String
+	imgurl: String,
+	imgbin: {data: Buffer, contentType: String}
 });
 
 module.exports = mongoose.model("Snap", snapSchema);

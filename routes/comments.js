f9ad33d6a46cf7789f064d05fc3c09f7bba95100
DeSteef <stefanpijnenburg@outlook.com>
2018-06-17 10:44:00 +0200

    add comment edit,destroy routes

--- routes/comments.js
+++ routes/comments.js
@@ -40,5 +40,47 @@ router.post("/snaps/:id/comments", function(req, res){
 	});
 });
 
+// EDIT ROUTE
+router.get("/snaps/:id/comments/:commentid/edit", function(req, res){
+	var snapID = req.params.id;
+	Snap.findById(snapID, function(err, snap){
+		if (err) {
+			console.log(err);
+			res.redirect("back");
+		} else {
+			Comment.findById(req.params.commentid, function(err, comment){
+				if (err) {
+					console.log(err);
+					res.redirect("back");
+				} else {
+					res.render("comments/edit", {comment:comment, snapID});
+				}
+			});
+		}
+	});
+});
+
+// UPDATE ROUTE
+router.put("/snaps/:id/comments/:commentid", function(req, res){
+	var commentID = req.params.commentid;
+	var data = req.body.comment;
+	Comment.findByIdAndUpdate(commentID, data, function(err, comment){
+		if (err) {
+			console.log(err);
+		}
+		res.redirect("/snaps/" + req.params.id);
+	});
+});
+
+// DESTROY ROUTE
+router.delete("/snaps/:id/comments/:commentid", function(req, res){
+	Comment.findByIdAndRemove(req.params.commentid, function(err, comment){
+		if (err) {
+			console.log(err);
+		}
+		res.redirect("/snaps/" + req.params.id);
+	});
+});
+
 
 module.exports = router;
